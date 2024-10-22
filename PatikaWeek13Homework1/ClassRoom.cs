05587f8afbce507ba95ad4628b2c2fd4c5c8f923
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-10-23 02:16:22 +0300

    Add project files.

---
+++ PatikaWeek13Homework1/ClassRoom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek13Homework1
+{
+    internal class ClassRoom
+    {
+        public readonly Teacher _teacher;
+        public ClassRoom(Teacher teacher)
+        {
+
+            _teacher = teacher; 
+
+        }
+
+        public  string GetTeacherInfo()
+        {
+            return _teacher.GetInfo();
+        }
+    }
+}
---
+++ PatikaWeek13Homework1/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek13Homework1
+{
+    internal class Teacher : IOgretmen
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string GetInfo()
+        {
+           return FirstName + " " + LastName;
+        }
+    }
+}
