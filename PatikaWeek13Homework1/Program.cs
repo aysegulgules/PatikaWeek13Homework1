ed169702eb9cef8aa0b61d97800fadc6bd904a85
ayşegül güleş <159700271+aysegulgules@users.noreply.github.com>
2024-10-23 02:51:51 +0300

    Create Program.cs

---
+++ PatikaWeek13Homework1/Program.cs
@@ -0,0 +1,41 @@
+
+using PatikaWeek13Homework1.Abstract;
+using PatikaWeek13Homework1.Conrete;
+
+namespace PatikaWeek13Homework1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddScoped<ITeacher,Teacher>(); 
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
